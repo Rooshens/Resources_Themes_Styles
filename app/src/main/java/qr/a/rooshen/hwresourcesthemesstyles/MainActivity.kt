95f217f0d8280edb561eff61749bc9bb75b1438e
aaaRooshen <aaa.rooshen@gmail.com>
2021-07-04 18:51:40 +0300

    hw

---
+++ app/src/main/java/qr/a/rooshen/hwresourcesthemesstyles/MainActivity.kt
@@ -0,0 +1,11 @@
+package qr.a.rooshen.hwresourcesthemesstyles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
